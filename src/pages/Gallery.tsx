00646bdc9e170a7fad3e82aef6d0b739bf6d9902
Haris Ali <harisali2533@gmail.com>
2026-05-06 11:46:02 -0400

    Images update

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,10 +1,7 @@
 const galleryImages = [
-    'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&w=1000&q=80',
-    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&w=1000&q=80',
-    'https://images.unsplash.com/photo-1519085360771-9852a60a8c45?auto=format&fit=crop&w=1000&q=80',
-    'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&w=1000&q=80',
-    'https://images.unsplash.com/photo-1553459459-d2229ba7433b?auto=format&fit=crop&w=1000&q=80',
-    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&w=1000&q=80',
+    '/gallery-1.jpg',
+    '/gallery-2.jpg',
+    'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=crop&w=1000&q=80',
 ];
 
 export default function Gallery() {
